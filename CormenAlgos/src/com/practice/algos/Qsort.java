9e53c2b2ee4906483dbc5fca34d7b83530478ff4
Clevins <vedisn@gmail.com>
2017-02-02 02:49:32 +1100

    Changed the comparison operator

--- CormenAlgos/src/com/practice/algos/Qsort.java
+++ CormenAlgos/src/com/practice/algos/Qsort.java
@@ -12,7 +12,7 @@ package com.practice.algos;
 public class Qsort {
 
     public static void main(String args[]) {
-        int ar[] = {13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 21};
+        int ar[] = {13,19,9,5,12,8,7,4,11,2,6,21};
         hoareSort(ar, 0, ar.length - 1);
        //int ar[]={8, 1, 3, 2, 5, 7, 6, 4};
 
@@ -30,9 +30,9 @@ public class Qsort {
 
     static void hoareSort(int ar[], int lo, int hi) {
         if (lo < hi) {
-            int p = lomutoPartition(ar, lo, hi);
-            lomutoPartition(ar, lo, p);
-            lomutoPartition(ar, p + 1, hi);
+            int p = hoarePartition(ar, lo, hi);
+            hoareSort(ar, lo, p);
+            hoareSort(ar, p + 1, hi);
         }
     }
 
@@ -64,13 +64,13 @@ public class Qsort {
         while (true) {
             do {
                 j = j - 1;
-            } while (ar[j] >= p);
+            } while (ar[j] > p);
 
             do {
                 i = i + 1;
-            } while (ar[i] <= p);
+            } while (ar[i] < p);
 
-            if (i <= j) {
+            if (i < j) {
                 swap(ar, i, j);
             } else {
                 return j;
